4bf6cb6558686683647f3d95aef8b449c880c730
Jami-K <87853545+Jami-K@users.noreply.github.com>
2022-10-11 16:06:20 +0900

    Update run.py

--- test/run.py
+++ test/run.py
@@ -2,34 +2,35 @@ import tensorflow as tf
 import cv2, os, hid, time
 import numpy as np
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
+#gpus = tf.config.experimental.list_physical_devices('GPU')
+#if gpus:
   # 텐서플로가 첫 번째 GPU에 1GB 메모리만 할당하도록 제한
-  try:
-    tf.config.experimental.set_virtual_device_configuration(gpus[0],
-        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=10000)])
-    tf.config.experimental.set_virtual_device_configuration(gpus[1],
-        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=10000)])
-    tf.config.experimental.set_virtual_device_configuration(gpus[2],
-        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=10000)])
-    tf.config.experimental.set_virtual_device_configuration(gpus[3],
-        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=10000)])
-  except RuntimeError as e:
+#  try:
+#    tf.config.experimental.set_virtual_device_configuration(gpus[0],
+#        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=15000)])
+#    tf.config.experimental.set_virtual_device_configuration(gpus[1],
+#        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=15000)])
+#    tf.config.experimental.set_virtual_device_configuration(gpus[2],
+#        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=15000)])
+#    tf.config.experimental.set_virtual_device_configuration(gpus[3],
+#        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=15000)])
+#  except RuntimeError as e:
     # 프로그램 시작시에 가상 장치가 설정되어야만 합니다
-    print(e)
+#    print(e)
 
 from tensorflow.keras.layers import Conv2D, Conv2DTranspose, Dense, Reshape, InputLayer, Flatten
 from alibi_detect.od import OutlierVAE
 from alibi_detect.utils.visualize import plot_instance_score, plot_feature_outlier_image
 from alibi_detect.utils.saving import save_detector, load_detector
 
+from model import LMS
 from relay import Relay
 from pypylon import pylon
 from multiprocessing import Process, Queue
 
 
 class Main:
-    def __init__(self, window_name, queue):
+    def __init__(self, window_name, model, queue):
         self.converter = pylon.ImageFormatConverter()
         self.config = tf.compat.v1.ConfigProto()
         self.IMG_SIZE1 = 64
@@ -41,12 +42,6 @@ class Main:
             self.info = {'window_name': window_name, 'GPU_id': '1',
                          'camera_property': 'camera_settingN.pfs',
                          'camera_id': 0, 'IMG_SIZE1': self.IMG_SIZE1, 'IMG_SIZE2': self.IMG_SIZE2}
-
-        print("Alibi Detect is ready...1")
-        self.od = self.make_network()
-        print("Alibi Detect is ready...2")
-        #self.od = load_detector("tray_od_20epochs.h5")
-        print("Alibi Detect is ready...3")
 
         self.load_camera()
         self.run()
@@ -91,34 +86,6 @@ class Main:
                         samples=4)
         return od
 
-
-    def load_camera(self):
-        maxCamerasToUse = 1
-        tlFactory = pylon.TlFactory.GetInstance()
-        devices = tlFactory.EnumerateDevices()
-
-        self.cameras = pylon.InstantCameraArray(min(len(devices), maxCamerasToUse))
-
-        for i, self.cam in enumerate(self.cameras):
-            self.cam.Attach(tlFactory.CreateDevice(devices[self.info['camera_id']]))
-
-        self.cameras.Open()
-        pylon.FeaturePersistence.Load(self.info['camera_property'], self.cam.GetNodeMap(), True)
-        # self.cameras.Close()
-
-        self.cameras.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
-        self.converter.OutputPixelFormat = pylon.PixelType_BGR8packed
-        self.converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
-
-    def get_img(self):
-        try:
-            self.grabResult = self.cameras.RetrieveResult(50000, pylon.TimeoutHandling_ThrowException)
-            image_raw = self.converter.Convert(self.grabResult)
-            img_raw = image_raw.GetArray()
-            img = cv2.cvtColor(img_raw, cv2.COLOR_BGR2RGB)
-            self.img = img
-        except:
-            pass
 
     def predict(self): #이미지 변환 방법 구현
         A = []
@@ -208,6 +175,7 @@ class Main:
                 pass
 
             self.get_img()
+            print("Picture Captured")
             output = self.predict()
             self.decide_ng(output)
             self.show_img()
@@ -223,146 +191,54 @@ class Main:
         cv2.destroyAllWindows()
 
 
-class Reject:
-    def __init__(self, queue1, queue2):
-        self.relay = Relay(idVendor=0x16c0, idProduct=0x05df)
-        self.relay.state(0, False)
+maxCamerasToUse = 1
+tlFactory = pylon.TlFactory.GetInstance()
+devices = tlFactory.EnumerateDevices()
 
-        self.relay_B = 'off'
-        self.relay_B_time = time.time()
+cameras = pylon.InstantCameraArray(min(len(devices), maxCamerasToUse))
 
-        self.relay_A_time = time.time()
-        self.relay_runtime = 0.05
-        self.relay_A = 'off'
-        self.Time_out = 0.001
-
-        self.reject_need_time = int(0.010 / self.Time_out) #10
-        self.standby_time = int(0.055 / self.Time_out) #55
-
-        self.queue1 = queue1
-        self.queue2 = queue2
-
-        self.T_A = [0] * (self.reject_need_time + self.standby_time)
-        self.T_B = [0] * (self.reject_need_time + self.standby_time)
-        print(self.T_A)
-
-        self.main()
-
-    def main(self):
-        """ Queue를 받아서 릴레이 가동을 위한 본 프로그램 """
-        initial_A, initial_B = 0, 0
-        while True:
-            answer = ''
-            ng_question_A = 'ok'
-            ng_question_B = 'ok'
-            try:
-                answer = self.queue1.get(timeout=self.Time_out)
-                if answer is not None:
-                    if answer[0][0] == 'R':
-                        #print('***** Reject Detected ****** ')
-                        if self.relay_A == 'off':
-                            self.T_A = self.time_traveler('A', 0, 1)
-                            #print('A : Reject signal put...')
-                            ng_question_A = 'ng'
-                        initial_A = 0
-
-            except:
-                pass
-
-            self.relay_off_cal_time()
-
-            if ng_question_A == 'ok':
-                self.T_A = self.time_traveler('A', 0, 0)
-                initial_A += 1
-            if ng_question_B == 'ok':
-                self.T_B = self.time_traveler('B', 0, 0)
-                initial_B += 1
-
-            if self.T_A[len(self.T_A) - 1] == 1:
-                self.state(1)
-            if self.T_B[len(self.T_B) - 1] == 1:
-                self.state(2)
-
-            if answer is None:
-                break
-
-    def time_traveler(self, line, location, pass_):
-        if line == 'A':
-            temp = self.T_A
-        else:
-            temp = self.T_B
-        temp = temp[:-1]
-        temp.insert(location, pass_)
-
-        return temp
-
-
-    def relay_off_cal_time(self):
-        if self.relay_A == 'on':
-            # print(time.time() - self.relay_A_time)
-            if time.time() - self.relay_A_time > self.relay_runtime:
-                self.relay.state(1, False)
-                self.relay_A = 'off'
-
-        if self.relay_B == 'on':
-            # print(time.time() - self.relay_A_time)
-            if time.time() - self.relay_B_time > self.relay_runtime:
-                self.relay.state(2, False)
-                self.relay_B = 'off'
-
-    def state(self, i):
-        if i == 1 and self.relay_A == 'off':
-            self.relay.state(1, True)
-            self.relay_A = 'on'
-            self.relay_A_time = time.time()
-            print("***** [[1]] Reject! *****")
-
-        elif i == 2 and self.relay_B == 'off':
-            self.relay.state(2, True)
-            self.relay_B = 'on'
-            self.relay_B_time = time.time()
-            print("***** [[2]] Reject! *****")
-
+for i, cam in enumerate(cameras):
+    cam.Attach(tlFactory.CreateDevice(devices[0]))
+cameras.Open()
+pylon.FeaturePersistence.Load('./camera_setting.pfs', cam.GetNodeMap())
+cameras.StartGrabbing(pylon.GrabStrategy_LatestImageOnly) 
+converter = pylon.ImageFormatConverter()
+converter.OutputPixelFormat = pylon.PixelType_BGR8packed
+converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
 
 if __name__ == "__main__":
     # 만약 save_foler가 없으면 폴더 만들기
     if not os.path.exists('Reject_img'):
         os.mkdir('Reject_img')
 
-    Q1, Q2 = Queue(), Queue()
-    relayqueue1 = Queue()
-    relayqueue2 = Queue()
+    L = LMS()
+    model = L.return_model()
+    model = load_detector("tray_od_20epochs.h5")
+    print("Alibi-detect Model Loaded...")
 
-    p1 = Process(target=Main, args=('TRAY', Q1,))
-    reject = Process(target=Reject, args=(relayqueue1, relayqueue2))
+    while cameras.IsGrabbing():
+        grabResult = cameras.RetrieveResult(50000, pylon.TimeoutHandling_ThrowException)
+        image_raw = converter.Convert(grabResult)
+        img_raw = image_raw.GetArray()
+        img_crop = img_raw[0:494, 0:494]
+        img = cv2.cvtColor(img_crop, cv2.COLOR_BGR2RGB)
+        img_resize = cv2.resize(img, (64, 64), interpolation=cv2.INTER_LINEAR)
+        A = img_resize.reshape((1,) + img_resize.shape)
+        A = A.astype('float32') / 255.
+        output = model.predict(A)
+        answer = output['data']['instance_score'][0]
+        is_outlier = output['data']['is_outlier'][0]
+        print("{} ..... / {}".format(answer,is_outlier))
 
-    p1.start()
-    reject.start()
+        cv2.imshow('example', img)
 
-    while True:
-        answer1, answer2 = '', ''
-        try:
-            answer1 = Q1.get(timeout=0.00001)
-            if answer1 is not None:
-                relayqueue1.put(answer1)
-        except:
-            pass
+        k = cv2.waitKey(1)
 
-        try:
-            answer2 = Q2.get(timeout=0.00001)
-            if answer2 is not None:
-                relayqueue2.put(answer2)
-        except:
-            pass
-
-        if answer1 is None:
-            Q2.put('off')
-            relayqueue1.put(None)
-            relayqueue2.put(None)
+        if k & 0xFF == 27:
             break
+     
+    cameras.Close()
+    cv2.destroyAllWindows()
 
-        if answer2 is None:
-            Q1.put('off')
-            relayqueue1.put(None)
-            relayqueue2.put(None)
-            break
+       
+ 
